2cc2d7cfec4d4152413f13301b765ee8c1f2e13b
Noelia Romina Ruiz <estefitrinidad@gmail.com>
2022-06-20 02:34:07 -0300

    segundo ingreso.rb al repo

---
+++ variables.rb
@@ -0,0 +1,11 @@
+#clase 20/6 - variables en ruby
+
+nombre = "Noelia"
+puts nombre
+nombre = 31
+puts nombre
+
+Nombre_user = "Noelia Romina" #las constantes arrancan con mayùsculas
+puts Nombre_user
+Nombre_user = "Nuevo nombre" #y no pueden cambiar su valor
+puts Nombre_user
